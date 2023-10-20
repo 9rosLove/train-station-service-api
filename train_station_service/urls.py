2633a7cdb19a315d0076e12b097f1c535bbd6a5a
9rosLove <yarik2300@gmail.com>
2023-10-21 00:09:51 +0300

    add urls.py to user app with register, login, me endpoints

--- train_station_service/urls.py
+++ train_station_service/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(
-        "api/train_station/",
-        include("train_station.urls", namespace="train_station"),
+    path("api/train_station/",
+         include("train_station.urls", namespace="train_station"),
     ),
+    path("api/user/", include("user.urls", namespace="user")),
 ]
