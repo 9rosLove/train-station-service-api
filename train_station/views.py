8c54f7d1054c61f6c0a835adea553128fad8bc62
9rosLove <yarik2300@gmail.com>
2023-10-22 00:54:34 +0300

    add Journey, Ticket, Order viewsets

--- train_station/views.py
+++ train_station/views.py
@@ -1,6 +1,15 @@
+from django.db.models import Count, F
 from rest_framework import viewsets
 
-from train_station.models import Crew, Station, Train, TrainType, Route
+from train_station.models import (
+    Crew,
+    Station,
+    Train,
+    TrainType,
+    Route,
+    Order,
+    Journey,
+)
 from train_station.serializers import (
     CrewSerializer,
     StationSerializer,
@@ -10,6 +19,12 @@ from train_station.serializers import (
     RouteListSerializer,
     RouteDetailSerializer,
     TrainListRetrieveSerializer,
+    OrderDetailSerializer,
+    OrderListSerializer,
+    OrderSerializer,
+    JourneyDetailSerializer,
+    JourneyListSerializer,
+    JourneySerializer,
 )
 
 
@@ -50,3 +65,46 @@ class TrainViewSet(viewsets.ModelViewSet):
         if self.action in ("list", "retrieve"):
             return TrainListRetrieveSerializer
         return self.serializer_class
+
+
+class JourneyViewSet(viewsets.ModelViewSet):
+    queryset = (
+        Journey.objects.all()
+        .select_related("route", "train")
+        .annotate(
+            tickets_available=(
+                F("train__cargo_number") * F("train__places_in_cargo")
+                - Count("tickets")
+            )
+        )
+    )
+    serializer_class = JourneySerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return JourneyListSerializer
+
+        if self.action == "retrieve":
+            return JourneyDetailSerializer
+
+        return self.serializer_class
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return OrderListSerializer
+
+        if self.action == "retrieve":
+            return OrderDetailSerializer
+
+        return self.serializer_class
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
