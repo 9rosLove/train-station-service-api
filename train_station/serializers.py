205969596bd134b92baa2380b2c6f4be5a1b6175
9rosLove <yarik2300@gmail.com>
2023-10-22 00:29:00 +0300

    add list and retrieve serializers for train and route views

--- train_station/serializers.py
+++ train_station/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from train_station.models import Crew, Station, TrainType, Train, Route
 
@@ -22,12 +23,38 @@ class TrainTypeSerializer(serializers.ModelSerializer):
 
 
 class RouteSerializer(serializers.ModelSerializer):
+    def validate(self, attrs):
+        data = super(RouteSerializer, self).validate(attrs)
+        Route.validate_station(data["source"], data["destination"], ValidationError)
+        return data
+
     class Meta:
         model = Route
-        fields = "__all__"
+        fields = ("id", "source", "destination", "distance_in_kilometers")
+
+
+class RouteListSerializer(RouteSerializer):
+    source = serializers.SlugRelatedField(slug_field="name", queryset=Station.objects.all())
+    destination = serializers.SlugRelatedField(slug_field="name", queryset=Station.objects.all())
+
+
+class RouteDetailSerializer(RouteSerializer):
+    source = StationSerializer(read_only=True)
+    destination = StationSerializer(read_only=True)
 
 
 class TrainSerializer(serializers.ModelSerializer):
+    def validate(self, attrs):
+        data = super(TrainSerializer, self).validate(attrs)
+        Train.validate_name(data["name"], ValidationError)
+        return data
+
     class Meta:
         model = Train
-        fields = "__all__"
+        fields = ("id", "name", "cargo_number", "places_in_cargo", "train_type", "capacity")
+
+
+class TrainListRetrieveSerializer(TrainSerializer):
+    train_type = serializers.SlugRelatedField(
+        slug_field="name", queryset=TrainType.objects.all()
+    )
